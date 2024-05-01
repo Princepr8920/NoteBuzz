18bd260757fdf00f2d6ab798533227f7b3608877
Prince <PrincePR8920@gmail.com>
2024-05-01 12:13:15 +0530

    tried to solve socket.io connection issue

--- src/services/socket.io/socketMiddleware.js
+++ src/services/socket.io/socketMiddleware.js
@@ -1,8 +1,7 @@
 const verifySocketToken = require("./socketVerification");
 const { addUser } = require("./socketOperations");
 
-async function SocketMiddleware(socket, next) {
-  console.log(socket.handshake.query.token,"Hello")
+async function SocketMiddleware(socket, next) { 
   const data = await verifySocketToken(socket.handshake.query.token);
   if (!data?.success) {
     return next(data); // Invalid token, reject the connection
